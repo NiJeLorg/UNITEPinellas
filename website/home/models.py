ffe4d8c9a87cebbf4273ce4c83eb25d562eb91d6
JD Godchaux <jd@nijel.org>
2019-03-20 12:45:19 -0500

    Updated blog pages, added about and profile page

--- website/home/models.py
+++ website/home/models.py
@@ -6,8 +6,10 @@ from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
+    headline = models.CharField(max_length=250)
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
+        FieldPanel('headline'),
         FieldPanel('body', classname="full"),
     ]
